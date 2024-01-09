383049e120db871e30b798cfde016b261e6d2a41
benlifurkan <furkanbenli33@gmail.com>
2024-01-09 22:12:24 +0300

    Update moments.vue with new messages and styling

--- src/pages/moments.vue
+++ src/pages/moments.vue
@@ -7,7 +7,21 @@ export default {
   data: () => ({
     messages: [
       {
+        from: "You",
+        message: `Sure, I'll see you later.`,
         time: "10:42am",
+        color: "deep-purple-lighten-1",
+      },
+      {
+        from: "John Doe",
+        message: "Yeah, sure. Does 1:00pm work?",
+        time: "10:37am",
+        color: "green",
+      },
+      {
+        from: "You",
+        message: "Did you still want to grab lunch today?",
+        time: "9:47am",
         color: "deep-purple-lighten-1",
       },
     ],
@@ -18,120 +32,30 @@ export default {
 <template>
   <v-container>
     <v-row justify="space-around">
-      <v-card class="mb-12" width="200">
+      <v-card width="400">
         <v-img
           height="200"
-          src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
+          src="https://cdn.discordapp.com/attachments/1175828283598848000/1193350390356394024/hello-kity-kolye.jpg?ex=65ac6528&is=6599f028&hm=fe6df89ab5769ba26ab4c69c13e049d3a15a715a885666a146a632040bbdce5e&"
           cover
           class="text-white"
         >
-          <!-- <v-toolbar color="rgba(0, 0, 0, 0)" theme="dark">
-            <template v-slot:prepend>
-              <v-btn icon="$menu"></v-btn>
-            </template>
-
-            <v-toolbar-title class="text-h6"> Messages </v-toolbar-title>
-
-            <template v-slot:append>
-              <v-btn icon="mdi-dots-vertical"></v-btn>
-            </template>
-          </v-toolbar> -->
         </v-img>
 
         <v-card-text>
-          <div class="font-weight-bold ms-1 mb-2">Today</div>
+          <div class="font-weight-bold ms-1 mb-2">7 Mayıs 2023</div>
 
           <v-timeline density="compact" align="start">
             <v-timeline-item
               v-for="message in messages"
               :key="message.time"
               :dot-color="message.color"
-              size="sm"
+              size="x-small"
             >
               <div class="mb-4">
                 <div class="font-weight-normal">
-                  <strong>{{ message.time }} </strong>
+                  <strong>{{ message.from }}</strong> @{{ message.time }}
                 </div>
-              </div>
-            </v-timeline-item>
-          </v-timeline>
-        </v-card-text>
-      </v-card>
-
-      <v-card class="mb-12" width="200">
-        <v-img
-          height="200"
-          src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
-          cover
-          class="text-white"
-        >
-          <!-- <v-toolbar color="rgba(0, 0, 0, 0)" theme="dark">
-            <template v-slot:prepend>
-              <v-btn icon="$menu"></v-btn>
-            </template>
-
-            <v-toolbar-title class="text-h6"> Messages </v-toolbar-title>
-
-            <template v-slot:append>
-              <v-btn icon="mdi-dots-vertical"></v-btn>
-            </template>
-          </v-toolbar> -->
-        </v-img>
-
-        <v-card-text>
-          <div class="font-weight-bold ms-1 mb-2">Today</div>
-
-          <v-timeline density="compact" align="start">
-            <v-timeline-item
-              v-for="message in messages"
-              :key="message.time"
-              :dot-color="message.color"
-              size="sm"
-            >
-              <div class="mb-4">
-                <div class="font-weight-normal">
-                  <strong>{{ message.time }} </strong>
-                </div>
-              </div>
-            </v-timeline-item>
-          </v-timeline>
-        </v-card-text>
-      </v-card>
-
-      <v-card class="mb-12" width="200">
-        <v-img
-          height="200"
-          src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
-          cover
-          class="text-white"
-        >
-          <!-- <v-toolbar color="rgba(0, 0, 0, 0)" theme="dark">
-            <template v-slot:prepend>
-              <v-btn icon="$menu"></v-btn>
-            </template>
-
-            <v-toolbar-title class="text-h6"> Messages </v-toolbar-title>
-
-            <template v-slot:append>
-              <v-btn icon="mdi-dots-vertical"></v-btn>
-            </template>
-          </v-toolbar> -->
-        </v-img>
-
-        <v-card-text>
-          <div class="font-weight-bold ms-1 mb-2">Today</div>
-
-          <v-timeline density="compact" align="start">
-            <v-timeline-item
-              v-for="message in messages"
-              :key="message.time"
-              :dot-color="message.color"
-              size="sm"
-            >
-              <div class="mb-4">
-                <div class="font-weight-normal">
-                  <strong>{{ message.time }} </strong>
-                </div>
+                <div>{{ message.message }}</div>
               </div>
             </v-timeline-item>
           </v-timeline>
