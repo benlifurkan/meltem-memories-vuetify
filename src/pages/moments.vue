3fea7822e992fdbecddb9dae6a60539af6fbe4f0
benlifurkan <furkanbenli33@gmail.com>
2024-01-12 06:00:57 +0300

    Update moments.vue with new messages

--- src/pages/moments.vue
+++ src/pages/moments.vue
@@ -5,11 +5,19 @@ import { ref, onMounted } from "vue";
 
 export default {
   data: () => ({
-    messages: [
+    messages_1: [
+      {
+        time: "18:42pm",
+        message: `Saçlarının rüzgarda uçuşmasını, içtiğin sigaranın dumanını, güldüğün zaman gözlerinin kapanmasını, kahkahalarını, konuşurken yaptığın mimikleri, kısacası her şeyini ilk kez gördüğüm gün.`,
+        color: "green-lighten-1",
+      },
+    ],
+
+    messages_2: [
       {
         time: "07:06am",
         message: `O gece seninle sabaha kadar film izleyip sohbet ettik. Kolyenin fotoğrafını çekmek için izin istedim ve sen de kabul ettin. O geceden kalan tek fotoğraf bu.`,
-        color: "green-lighten-1",
+        color: "purple-lighten-1",
       },
     ],
   }),
@@ -29,20 +37,50 @@ export default {
         </v-img>
 
         <v-card-text>
+          <div class="font-weight-bold mb-2">6 Mayıs 2023</div>
+
+          <v-timeline density="compact" align="center">
+            <v-timeline-item
+              v-for="message in messages_1"
+              :key="message.time"
+              :dot-color="message.color"
+              size="12"
+            >
+              <div class="mt-0">
+                <div class="font-weight-bold mb-2">
+                  <strong>@{{ message.time }}</strong>
+                </div>
+                <div class="mb-2">{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+
+      <v-card width="200" height="500px">
+        <v-img
+          height="175"
+          src="/public/moments/card-image-2.png"
+          cover
+          class="text-white"
+        >
+        </v-img>
+
+        <v-card-text>
           <div class="font-weight-bold mb-2">7 Mayıs 2023</div>
 
           <v-timeline density="compact" align="center">
             <v-timeline-item
-              v-for="message in messages"
+              v-for="message in messages_2"
               :key="message.time"
               :dot-color="message.color"
-              size="15"
+              size="12"
             >
               <div class="mt-2">
                 <div class="font-weight-bold mb-2">
                   <strong>@{{ message.time }}</strong>
                 </div>
-                <div>{{ message.message }}</div>
+                <div class="mb-2">{{ message.message }}</div>
               </div>
             </v-timeline-item>
           </v-timeline>
