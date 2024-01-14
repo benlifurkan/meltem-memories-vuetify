a5a84cbe663b3ad85f83fb8e91581aa9f2047e33
benlifurkan <furkanbenli33@gmail.com>
2024-01-14 17:00:52 +0300

    Refactor theme toggle button in App.vue

--- src/App.vue
+++ src/App.vue
@@ -92,18 +92,12 @@ function toggleTheme() {
       <VAppBar v-else flat border>
         <template #prepend>
           <RouterLink
-            v-if="theme.global.current.value.dark"
             to="/"
-            class="text-white px-4 py-2 rounded-xl sm text-decoration-none"
-            variant="accent"
-          >
-            <VIcon icon="mdi-heart" class="mr-2" />
-            <span class="text-xs">Meltem Moments</span>
-          </RouterLink>
-          <RouterLink
-            v-else
-            to="/"
-            class="text-black px-4 py-2 rounded-xl sm text-decoration-none"
+            :class="{
+              btn: true,
+              'dark text-white': theme.current.value.dark,
+              'light text-black': theme.global.name.value,
+            }"
             variant="accent"
           >
             <VIcon icon="mdi-heart" class="mr-2" />
