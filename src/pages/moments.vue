248be2dbea2c61cc05f2e60ca81bd2e22978a65e
benlifurkan <furkanbenli33@gmail.com>
2024-01-14 03:33:34 +0300

    Update moments.vue component with new timeline messages

--- src/pages/moments.vue
+++ src/pages/moments.vue
@@ -23,8 +23,24 @@ export default {
 
     messages_3: [
       {
+        time: "15:40pm",
+        message: `O gün seninle ilk kez bir parka gidip oturduk. Seninle ilk kez bir şeyler yaptık. Seninle ilk kez garip şeyler hissettik. Seni ilk kez öptüm. Bu hisler benim için çok yeniydi. Bu güzel hisleri ilk kez senin gibi mükemmel biriyle yaşadığım için çok şanslıyım.`,
+        color: "pink-lighten-1",
+      },
+    ],
+
+    messages_4: [
+      {
         time: "17:13pm",
         message: `Senden kilometrelerce uzakta olmama rağmen, doğum günün için güzel bir şeyler yapmam gerekiyordu. Bu yüzden sana hayatın boyunca unutamayacağın bir sürpriz hazırladım. İlk defa birinin doğum günü için bu kadar çaba sarfettim.`,
+        color: "yellow-lighten-1",
+      },
+    ],
+
+    messages_5: [
+      {
+        time: "17:53pm",
+        message: `Uzun bir uzak mesafe ilişkisinden sonra o gün seninle ilk kez buluştuk. Sana sarılmayı, kokunu içime çekmeyi, ellerini tutmayı çok özlediğimi iliklerime kadar hissetmiştim. Uzun bir ayrılıktan sonra seni tekrar görmek bana çok iyi geldi. `,
         color: "yellow-lighten-1",
       },
     ],
@@ -105,11 +121,163 @@ export default {
         </v-img>
 
         <v-card-text>
-          <div class="font-weight-bold mb-2">17 Mayıs 2023</div>
+          <div class="font-weight-bold mb-2">7 Mayıs 2023</div>
 
           <v-timeline density="compact" align="center">
             <v-timeline-item
               v-for="message in messages_3"
+              :key="message.time"
+              :dot-color="message.color"
+              size="12"
+            >
+              <div class="">
+                <div class="font-weight-bold mb-2">
+                  <strong>@{{ message.time }}</strong>
+                </div>
+                <div class="">{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+
+      <v-card class="mb-8" width="250" height="500px">
+        <v-img
+          height="175"
+          src="/moments/card-image-4.png"
+          cover
+          class="text-white"
+        >
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold mb-2">17 Mayıs 2023</div>
+
+          <v-timeline density="compact" align="center">
+            <v-timeline-item
+              v-for="message in messages_4"
+              :key="message.time"
+              :dot-color="message.color"
+              size="12"
+            >
+              <div class="">
+                <div class="font-weight-bold mb-2">
+                  <strong>@{{ message.time }}</strong>
+                </div>
+                <div class="">{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+
+    <v-row justify="space-around">
+      <v-card class="mb-8" width="250" height="500px">
+        <v-img
+          height="175"
+          src="/moments/card-image-5.png"
+          cover
+          class="text-white"
+        >
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold mb-2">9 Haziran 2023</div>
+
+          <v-timeline density="compact" align="center">
+            <v-timeline-item
+              v-for="message in messages_5"
+              :key="message.time"
+              :dot-color="message.color"
+              size="12"
+            >
+              <div class="mt-0">
+                <div class="font-weight-bold mb-2">
+                  <strong>@{{ message.time }}</strong>
+                </div>
+                <div class="mb-2">{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+
+      <v-card class="mb-8" width="250" height="500px">
+        <v-img
+          height="175"
+          src="/moments/card-image-2.png"
+          cover
+          class="text-white"
+        >
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold mb-2">7 Mayıs 2023</div>
+
+          <v-timeline density="compact" align="center">
+            <v-timeline-item
+              v-for="message in messages_2"
+              :key="message.time"
+              :dot-color="message.color"
+              size="12"
+            >
+              <div class="mt-2">
+                <div class="font-weight-bold mb-2">
+                  <strong>@{{ message.time }}</strong>
+                </div>
+                <div class="mb-2">{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+
+      <v-card class="mb-8" width="250" height="500px">
+        <v-img
+          height="175"
+          src="/moments/card-image-3.png"
+          cover
+          class="text-white"
+        >
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold mb-2">7 Mayıs 2023</div>
+
+          <v-timeline density="compact" align="center">
+            <v-timeline-item
+              v-for="message in messages_3"
+              :key="message.time"
+              :dot-color="message.color"
+              size="12"
+            >
+              <div class="">
+                <div class="font-weight-bold mb-2">
+                  <strong>@{{ message.time }}</strong>
+                </div>
+                <div class="">{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+
+      <v-card class="mb-8" width="250" height="500px">
+        <v-img
+          height="175"
+          src="/moments/card-image-4.png"
+          cover
+          class="text-white"
+        >
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold mb-2">17 Mayıs 2023</div>
+
+          <v-timeline density="compact" align="center">
+            <v-timeline-item
+              v-for="message in messages_4"
               :key="message.time"
               :dot-color="message.color"
               size="12"
